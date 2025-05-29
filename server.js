3c77078d0e8ecbad9abb58e232aaa61bd69e3353
hellomyworld123 <pozip5515@gmail.com>
2025-05-29 04:14:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 const corsOptions = {
-  origin: ['https://sahar-frontend.vercel.app', 'http://localhost:3000'],
+  origin: ['https://sahar-frontend.vercel.app', 'https://nail-care-frontend.vercel.app', 'http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
